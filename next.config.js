5b13ab226faa3514b3fd2be610fff40bec66e971
IvailoStoyanov <ivailo.stoyanov.work@gmail.com>
2023-11-07 15:07:40 +0200

    increase static page generation timeout

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  // time in seconds of no pages generating during static generation before timing out
+  staticPageGenerationTimeout: 1000,
 }
 
 module.exports = nextConfig
